4197fc31dc1ef7ebcd5e7c87e25ae5c51fec20be
jbalcita <jbalcita@cj.com>
2018-02-28 11:56:56 -0800

    formatting

--- elm-architecture/ArchitectedHello.elm
+++ elm-architecture/ArchitectedHello.elm
@@ -4,56 +4,67 @@ import Html exposing (..)
 import Html.Attributes exposing (style, value)
 import Html.Events exposing (onClick, onInput)
 
+
 main : Program Never Model Msg
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+    Html.beginnerProgram { model = model, view = view, update = update }
 
--- The shape of our model
+
+
+
+
 type alias Model =
-  { text : String
-  , size : Int
-  , inputText : String
-  }
+    { text : String
+    , size : Int
+    , inputText : String
+    }
+
 
 model : Model
 model =
-  { text = "Hello world"
-  , size = 1
-  , inputText = ""
-  }
+    { text = "Hello world"
+    , size = 1
+    , inputText = ""
+    }
+
 
 type Msg
-  = Text String
-  | SizeUp
-  | SizeDown
-  | Input String
+    = Text String
+    | SizeUp
+    | SizeDown
+    | Input String
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Text str ->
-      { model | text = model.text ++ str }
-    SizeUp ->
-      { model | size = model.size + 1}
-    SizeDown ->
-      { model | size = model.size - 1}
-    Input str ->
-      { model | inputText = str }
+    case msg of
+        Text str ->
+            { model | text = model.text ++ str }
+
+        SizeUp ->
+            { model | size = model.size + 1 }
+
+        SizeDown ->
+            { model | size = model.size - 1 }
+
+        Input str ->
+            { model | inputText = str }
+
 
 myStyle : Int -> Attribute msg
 myStyle size =
-  style
-  [ ("fontSize", toString size ++ "em")
-  , ("color", "teal")
-  ]
+    style
+        [ ( "fontSize", toString size ++ "em" )
+        , ( "color", "teal" )
+        ]
+
 
 view : Model -> Html Msg
 view model =
-  div [] [
-    div [ myStyle model.size ] [ text model.text ]
-    , input [ onInput (Input) ] []
-    , button [ onClick (Text model.inputText) ] [ text "Add text to model" ]
-    , button [ onClick (SizeUp) ] [ text "+" ]
-    , button [ onClick (SizeDown) ] [ text "-" ]
-  ]
+    div []
+        [ div [ myStyle model.size ] [ text model.text ]
+        , input [ onInput Input ] []
+        , button [ onClick (Text model.inputText) ] [ text "Add text to model" ]
+        , button [ onClick SizeUp ] [ text "+" ]
+        , button [ onClick SizeDown ] [ text "-" ]
+        ]
