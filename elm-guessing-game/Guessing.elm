0cd5c5acd61954b50ffdac8898e0b6b1f1c7c60e
jbalcita <jbalcita@cj.com>
2018-03-01 13:52:23 -0800

    basic guessing functionality
    - submit input form and get result

--- elm-guessing-game/Guessing.elm
+++ elm-guessing-game/Guessing.elm
@@ -2,33 +2,57 @@ module Guessing exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput, onClick, onSubmit)
 
 
+main : Program Never Model Msg
 main =
     Html.beginnerProgram { model = model, view = view, update = update }
 
 
 type alias Model =
-    { displayText : String }
+    { word : String
+    , guess : String
+    , isCorrect : Bool
+    }
 
 
 type Msg
-    = Text String
+    = Answer
+    | Change String
 
 
 model : Model
 model =
-    { displayText = "Guessing Game"
-    }
+    Model "Saturday" "" False
 
 
 view : Model -> Html Msg
 view model =
-    div [] [ text model.displayText ]
+    div [] [
+        h2 [] [ text (clueOrReveal model) ]
+      , Html.form [ onSubmit (Answer) ] [
+            input [ placeholder "Write your guess here", value model.guess, onInput Change ] []
+        ]
+    ]
 
 
-update : Msg -> { b | displayText : a } -> { b | displayText : String }
+update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Text str ->
-            { model | displayText = str }
+        Answer ->
+            { model | isCorrect = (checkIfCorrect model) }
+        Change str ->
+            { model | guess = str }
+
+
+checkIfCorrect : Model -> Bool
+checkIfCorrect model =
+    model.guess == model.word
+
+clueOrReveal : Model -> String
+clueOrReveal model =
+    if model.isCorrect then
+        "You guessed right! The answer is " ++ model.word
+    else
+        "I'm thinking of a word that begins with " ++ String.slice 0 1 model.word
