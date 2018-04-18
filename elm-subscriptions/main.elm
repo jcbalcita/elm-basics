59fbb2b591ff55c6631a9e4157607d24a17398bf
jbalcita <jbalcita@cj.com>
2018-04-18 16:33:37 -0700

    formatting

--- elm-subscriptions/main.elm
+++ elm-subscriptions/main.elm
@@ -1,63 +1,69 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, text, program)
+import Html exposing (Html, div, program, text)
 import Html.Attributes exposing (style)
-import Mouse exposing (moves, clicks)
+import Mouse exposing (clicks, moves)
+
 
 main =
-  program
-  { init = init
-  , view = view
-  , update = update
-  , subscriptions = subscriptions
-  }
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 type alias Model =
-  { x : Int
-  , y : Int
-  , color : String
-  }
+    { x : Int
+    , y : Int
+    , color : String
+    }
 
 
 type Msg
-  = MoveMessage Mouse.Position
-  | ClickMessage Mouse.Position
+    = MoveMessage Mouse.Position
+    | ClickMessage Mouse.Position
 
 
 init =
-  ( Model 0 0 "red", Cmd.none)
+    ( Model 0 0 "red", Cmd.none )
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    MoveMessage position ->
-      ({model | x = position.x, y = position.y}, Cmd.none)
+    case msg of
+        MoveMessage position ->
+            ( { model | x = position.x, y = position.y }, Cmd.none )
 
-    ClickMessage _ ->
-      ({model | color = changeColor model}, Cmd.none)
+        ClickMessage _ ->
+            ( { model | color = changeColor model }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.batch
-    [ Mouse.moves MoveMessage
-    , Mouse.clicks ClickMessage
-    ]
+    Sub.batch
+        [ Mouse.moves MoveMessage
+        , Mouse.clicks ClickMessage
+        ]
 
 
 changeColor : Model -> String
 changeColor { color } =
-  case color of
-    "blue" -> "red"
-    "red" -> "blue"
-    _ -> "red"
+    case color of
+        "blue" ->
+            "red"
+
+        "red" ->
+            "blue"
+
+        _ ->
+            "red"
 
 
 view : Model -> Html Msg
 view model =
-  div [ style [( "color", model.color )] ]
-  [ text
-      ((toString model.x) ++ " " ++ (toString model.y))
-  ]
+    div [ style [ ( "color", model.color ) ] ]
+        [ text
+            (toString model.x ++ " " ++ toString model.y)
+        ]
